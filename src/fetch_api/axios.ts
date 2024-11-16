2d1dd0e613358766ebf083a7374d4d03ad1ed777
Jahongir <95927694+JFaithTrust@users.noreply.github.com>
2024-11-16 15:07:50 +0500

    Change base url

--- src/fetch_api/axios.ts
+++ src/fetch_api/axios.ts
@@ -4,13 +4,13 @@ import axios, { AxiosInstance } from "axios";
 // const BASE_URL = "http://172.16.14.23:9090";
 
 // local url
-const BASE_URL = "https://guinea-ideal-cod.ngrok-free.app";
+const BASE_URL = process.env.NEXT_PUBLIC_GLOBAL_API_URL!;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    "ngrok-skip-browser-warning": "any",
-  },
+  // headers: {
+  //   "ngrok-skip-browser-warning": "any",
+  // },
 });
 
 export default axiosInstance;
