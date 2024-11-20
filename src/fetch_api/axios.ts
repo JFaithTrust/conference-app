c06a032dedb5e90b0ed79049aaa644e8d0a2f3a5
Jahongir <95927694+JFaithTrust@users.noreply.github.com>
2024-11-20 20:32:42 +0500

    change: base url payment uz

--- src/fetch_api/axios.ts
+++ src/fetch_api/axios.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from "axios";
 // const BASE_URL = "http://172.16.14.23:9090";
 
 // local url
-const BASE_URL = "https//payment.tuit.uz";
+const BASE_URL = "https://payment.tuit.uz";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
