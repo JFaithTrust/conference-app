c2c69b8e830c3da4a62f369ead1ff9ec2f2947e5
Jahongir <95927694+JFaithTrust@users.noreply.github.com>
2024-12-17 12:04:31 +0500

    change: partners

--- src/app/(home)/home_components/Partners.tsx
+++ src/app/(home)/home_components/Partners.tsx
@@ -1,46 +1,47 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 
 import "swiper/css";
-import { FC } from "react";
+import {FC} from "react";
 
-import { Autoplay } from "swiper/modules";
+import {Autoplay} from "swiper/modules";
 
 interface Item {
-  image: string;
+    image: string;
+    classNames: string;
 }
 
 interface IProps {
-  items: Item[];
+    items: Item[];
 }
 
-const Partners: FC<IProps> = ({ items }) => {
-  return (
-    <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-        className="mySwiper"
-      >
-        {items.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className="flex items-center justify-center relative w-[280px] h-[79px]">
-              <img src={item.image} alt="" className="" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
-  );
+const Partners: FC<IProps> = ({items}) => {
+    return (
+        <>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className="mySwiper"
+            >
+                {items.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="flex items-center justify-center relative w-[280px] h-[79px]">
+                            <img src={item.image} alt="" className={item.classNames}/>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
+    );
 };
 
 export default Partners;
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,31 +1,48 @@
-import { ChildProps } from "@/types";
-import React, { Suspense } from "react";
-import { LoginModal, RegisterModal } from "@/components/modals";
-import { Footer, Navbar, Partners } from "./home_components";
+import {ChildProps} from "@/types";
+import React, {Suspense} from "react";
+import {LoginModal, RegisterModal} from "@/components/modals";
+import {Footer, Navbar, Partners} from "./home_components";
 import ForgotPasswordModal from "@/components/modals/ForgotPasswordModal";
 
-const HomeLayout = ({ children }: ChildProps) => {
-  const items = [
-    { image: "/images/image_1.png" },
-    { image: "/images/image_2.png" },
-    { image: "/images/innovation.png" },
-    { image: "/images/oliy.png" },
-  ];
-  return (
-    <div className="min-h-screen w-full">
-      <ForgotPasswordModal />
-      <RegisterModal />
-      <LoginModal />
-      <Navbar />
-      <main className="w-full min-h-screen">{children}</main>
-      <div className="w-full flex justify-end py-[20px]">
-        <div className="rounded-tl-2xl rounded-tr-none rounded-br-none rounded-bl-2xl bg-herowhite w-[1600px] h-[122px] flex flex-row items-center justify-start py-5 px-[50px] box-border gap-[50px]">
-          <Partners items={items} />
+const HomeLayout = ({children}: ChildProps) => {
+    const items = [
+        {
+            image: "/images/image_1.png",
+            classNames: "w-20"
+        },
+        {
+            image: "/images/innovation.png",
+            classNames: "w-44"
+        },
+        {
+            image: "/images/oliy.png",
+            classNames: "w-24"
+        },
+        {
+            image: "/images/nova.png",
+            classNames: "w-40"
+        },
+        {
+            image: "/images/raqamli_texnologiyalar.png",
+            classNames: "w-20"
+        },
+    ];
+    return (
+        <div className="min-h-screen w-full">
+            <ForgotPasswordModal/>
+            <RegisterModal/>
+            <LoginModal/>
+            <Navbar/>
+            <main className="w-full min-h-screen">{children}</main>
+            <div className="w-full flex justify-end py-[20px]">
+                <div
+                    className="rounded-tl-2xl rounded-tr-none rounded-br-none rounded-bl-2xl bg-herowhite w-[1600px] h-[122px] flex flex-row items-center justify-start py-5 px-[50px] box-border gap-[50px]">
+                    <Partners items={items}/>
+                </div>
+            </div>
+            <Footer/>
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default HomeLayout;
